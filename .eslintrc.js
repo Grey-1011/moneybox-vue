090b0919ab722691419694ea72abce7ca1392681
grey <z1251543107@gmail.com>
2022-02-26 15:48:14 +0800

    完成Types组件

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,7 +16,8 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    "@typescript-eslint/explicit-module-boundary-types": "off"
   },
   overrides: [
     {
@@ -24,6 +25,9 @@ module.exports = {
         '**/__tests__/*.{j,t}s?(x)',
         '**/tests/unit/**/*.spec.{j,t}s?(x)'
       ],
+      "rules": {
+        "@typescript-eslint/explicit-module-boundary-types": ["error"]
+      },
       env: {
         jest: true
       }
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,7 +1,7 @@
 <template>
   <layout class-prefix="layout">
     <NumberPad/>
-    <Types/>
+    <Types :xxx="111"/>
     <Notes/>
     <Tags/>
   </layout>
