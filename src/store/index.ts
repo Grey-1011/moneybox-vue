922f2f41da1b06d009a266b93fea993510dba495
grey <z1251543107@gmail.com>
2022-05-21 11:07:40 +0800

    删除无用文件

--- src/store/index.ts
+++ src/store/index.ts
@@ -56,7 +56,7 @@ const store = new Vuex.Store({
         },
         createRecord(state, record: RecordItem) {
             const record2: RecordItem = clone(record);
-            record2.createdAt = new Date().toISOString();
+            record2.createdAt = record2.createdAt || new Date().toISOString()
             state.recordList.push(record2);// 可选链语法 this.recordList && this.recordList.push(record2);
             store.commit('saveRecords');
         },
